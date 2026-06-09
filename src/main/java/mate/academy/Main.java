4f46aea453b8dcb335487f4c7b3d1368f1664177
obvly <zazedu4@gmail.com>
2026-06-10 01:41:52 +0200

    test

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -8,7 +8,8 @@ public class Main {
     private static final Injector injector = Injector.getInstance("mate.academy");
 
     public static void main(String[] args) {
-        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        MovieService movieService = (MovieService)
+                injector.getInstance(MovieService.class);
 
         Movie movie = new Movie();
         movie.setTitle("Inception");

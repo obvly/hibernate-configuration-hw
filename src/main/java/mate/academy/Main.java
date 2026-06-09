822f8f88824b296cabca45a59e637f17bfec0b95
obvly <zazedu4@gmail.com>
2026-06-10 01:33:36 +0200

    Made changes

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,22 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+
+        Movie movie = new Movie();
+        movie.setTitle("Inception");
+        movie.setDescription("Dream within a dream");
+
+        movieService.add(movie);
+
+        Movie movieFromDb = movieService.get(movie.getId());
+        System.out.println(movieFromDb);
     }
 }
